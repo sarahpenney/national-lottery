efddbdbe5e54af597f7eaf2e4bcf9cd7ad1d8bf5
Sarah Penney <sarahpenneyuk@gmail.com>
2017-10-01 15:59:54 +0100

    ADDED: initial commit

---
+++ app.js
@@ -0,0 +1,62 @@
+console.log('loaded');
+
+
+var lottoApp = angular.module('lottoApp', ['ngRoute']);
+lottoApp.controller('mainController', function($scope) {
+  $scope.message = 'test angular message';
+});
+
+
+// configure our routes
+lottoApp.config(function($routeProvider) {
+  $routeProvider
+
+            // route for the home page
+      .when('/', {
+        templateUrl: 'pages/home.html',
+        controller: 'mainController'
+      })
+
+      .when('/begin', {
+        templateUrl: 'pages/begin.html',
+        controller: 'beginController'
+      })
+
+        .when('/calculating', {
+          templateUrl: 'pages/calculating.html',
+          controller: 'calculatingController'
+        })
+
+        .when('/winner', {
+          templateUrl: 'pages/winner.html',
+          controller: 'winnerController'
+        })
+
+        .when('/looser', {
+          templateUrl: 'pages/looser.html',
+          controller: 'looserController'
+        });
+
+
+});
+
+    // create the controller and inject Angular's $scope
+lottoApp.controller('mainController', function($scope) {
+  $scope.message = 'main';
+});
+
+lottoApp.controller('beginController', function($scope) {
+  $scope.message = 'begin';
+});
+
+lottoApp.controller('calculatingController', function($scope) {
+  $scope.message = 'calc';
+});
+
+lottoApp.controller('winnerController', function($scope) {
+  $scope.message = 'win';
+});
+
+lottoApp.controller('looserController', function($scope) {
+  $scope.message = 'loser';
+});
