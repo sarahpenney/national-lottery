2a5307e78d094eb91a1af2242a22a2c9e55cac63
Sarah Penney <sarahpenneyuk@gmail.com>
2017-10-04 18:10:55 +0100

    ADDED: dummy data

--- app.js
+++ app.js
@@ -1,17 +1,10 @@
 console.log('loaded');
-
-
 var lottoApp = angular.module('lottoApp', ['ngRoute']);
-lottoApp.controller('mainController', function($scope) {
-  $scope.message = 'test angular message';
-});
-
 
 // configure our routes
 lottoApp.config(function($routeProvider) {
   $routeProvider
 
-            // route for the home page
       .when('/', {
         templateUrl: 'pages/home.html',
         controller: 'mainController'
@@ -27,16 +20,14 @@ lottoApp.config(function($routeProvider) {
           controller: 'winnerController'
         })
 
-        .when('/looser', {
-          templateUrl: 'pages/looser.html',
-          controller: 'looserController'
+        .when('/loser', {
+          templateUrl: 'pages/loser.html',
+          controller: 'loserController'
         });
-
-
 });
 
     // create the controller and inject Angular's $scope
-lottoApp.controller('mainController', function($scope, $http) {
+lottoApp.controller('mainController', function($scope, $http, $timeout, $location) {
   $scope.message = 'main';
 
   var playernumbers = [];
@@ -44,21 +35,57 @@ lottoApp.controller('mainController', function($scope, $http) {
   $scope.update = function(user) {
     $scope.master = angular.copy(user);
 
-    // for matching lotto data
-    $scope.playernumbers = [user.number1, user.number2, user.number3, user.number4, user.number5, user.number6]
-
+    // for matching lotto data: get numbers in array and sort from low to high
+    $scope.playernumbersinput = [user.number1, user.number2, user.number3, user.number4, user.number5, user.number6]
+    $scope.playernumbers = $scope.playernumbersinput.sort(function(a, b){return a-b});
     console.log('playernumbers', $scope.playernumbers);
-
-    // for html bind use
+    // for html use
     $scope.numbersinstring = $scope.playernumbers.toString();
     console.log($scope.numbersinstring);
 
+    // get the data to match numbers
+    $http.get('/dummy-data.json')
+          .success(function (data) {
+            $scope.data = data;
+            angular.forEach($scope.data, function(item){
+              console.log($scope.data);
+              console.log(item.numbers);
+              $scope.numberstomatch = item.numbers
+            });
+          })
+          .error(function (data) {
+            console.log('err');
+          });
+
+
+    $timeout( function(){
+
+      // angular.forEach($scope.playernumbersinput, function(numbers) {
+      //   angular.forEach($scope.numberstomatch, function(numbers) {
+      //     if ($scope.playernumbersinput === $scope.numberstomatch) {
+      //       $location.path("pages/winner.html");
+      //     }
+      //   });
+      // });
+
+
+      if ($scope.playernumbersinput === $scope.numberstomatch) {
+        $location.path("pages/winner.html");
+      } else {
+        $location.path("pages/loser.html");
+      }
+
+    }, 4000);
+
   };
 
+  $scope.second = false;
+  $scope.first = true;
 
-
-
-
+  $scope.showAlert = function(){
+    $scope.second = true;
+    $scope.first = false;
+  };
 
 
 });
@@ -67,6 +94,6 @@ lottoApp.controller('winnerController', function($scope) {
   $scope.message = 'win';
 });
 
-lottoApp.controller('looserController', function($scope) {
+lottoApp.controller('loserController', function($scope) {
   $scope.message = 'loser';
 });
